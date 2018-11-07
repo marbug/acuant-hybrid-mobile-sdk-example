d1747a40d2d68424ca3feca5f20a4358657ba2f7
Maryan Bagnyuk <marbuga@gmail.com>
2018-11-07 20:59:12 +0200

    add scripts

---
+++ scripts/add-dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ionic cordova plugin add cordova-plugin-compat
+ionic cordova plugin add cordova-plugin-console
+ionic cordova plugin add sdgc-cordova-native-dialogs
+ionic cordova plugin add cordova-plugin-network-information
+ionic cordova plugin add cordova-plugin-whitelist
+ionic cordova plugin add cordova-plugin-intent
+ionic cordova plugin add ../../AcuantHybridMobileSDK/cordova-plugin-AcuantHybridSDK --nofetch
+ionic cordova platform add android
---
+++ scripts/create-project-from-scretch.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ionic start MyTestIonicApp blank
